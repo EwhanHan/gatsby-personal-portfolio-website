62772e746d6b66171d4aa6911226ccd82f8ae3d8
Ewhan Han <ewhan.han@gmail.com>
2022-04-28 23:33:42 -0400

    Updates

--- src/utils/context/usercontext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-const UserContext = createContext({
-  isSidebarActive: false,
-  showSidebar: () => {},
-  hideSidebar: () => {},
-});
-
-export { UserContext };
