fc14345b2d63becc877de5f4fec21ca8483e0efb
Ewhan Han <ewhan.han@gmail.com>
2022-05-02 19:14:46 -0400

    Update StyleWrapper

--- src/components/stylewrapper/stylewrapper.js
+++ src/components/stylewrapper/stylewrapper.js
@@ -3,7 +3,9 @@ import React from 'react';
 const stylewrapper = props => {
   return (
     <section className='hero'>
-      <div className='hero__content'>{props.children}</div>
+      <article>
+        <div className='hero__content'>{props.children}</div>
+      </article>
     </section>
   );
 };
